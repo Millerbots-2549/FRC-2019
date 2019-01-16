03b098974975548af12877b10f7e9c5fd7b70e25
viclabV1 <vplabrie@gmail.com>
2019-01-16 15:49:37 -0600

    Adds drive command

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,11 +1,11 @@
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class DriveCommand extends Command {
     public DriveCommand() {
-        // Use requires() here to declare subsystem dependencies
+       super (DriveCommand.class.getSimpleName());
         requires(Robot.m_drivetrain);
     }
 
@@ -17,6 +17,8 @@ public class DriveCommand extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+      //  Robot.m_drivetrain.drive(Robot.m_oi.getDriveL(), Robot.m_oi.getDriveR());
+ //       Robot.m_drivetrain.;
     }
 
     // Make this return true when this Command no longer needs to run execute()
