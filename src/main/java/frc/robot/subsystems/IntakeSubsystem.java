6a9191b3fa8677e0e3c1847b44f54281defe107c
Millerbots-2549 <millerbots@gmail.com>
2019-01-22 16:37:52 -0600

    added IntakeSubsystem :)

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class IntakeSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    // TODO: Create Solenoid
+
+    // TODO: Create Motor
+
+
+    public void initDefaultCommand() {
+        // TODO: Set the default command, if any, for a subsystem here. Example:
+        //    setDefaultCommand(new MySpecialCommand());
+    }
+}
+
