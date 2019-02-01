a63b7e9348f31e370929c83ba9a4f93bd0382a51
2549-Elitebook <millerbots@gmail.com>
2019-02-01 16:34:44 -0600

    Merge branch 'dev' of https://github.com/Millerbots-2549/FRC-2019 into dev
    # Conflicts:
    #	.idea/libraries/Gradle__com_ctre_phoenix_api_java_5_12_1.xml
    #	.idea/libraries/Gradle__com_ctre_phoenix_wpiapi_java_5_12_1.xml
    #	.idea/modules/FRC-2019_main.iml
    #	src/main/java/frc/robot/OI.java
    #	src/main/java/frc/robot/Robot.java
    #	src/main/java/frc/robot/commands/HatchCommand.java
    #	src/main/java/frc/robot/subsystems/HatchSubsystem.java

--- src/main/java/frc/robot/subsystems/ClimbSubsytem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsytem.java
@@ -2,16 +2,19 @@ package frc.robot.subsystems;
 
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import frc.robot.RobotMap;
+import frc.robot.commands.ClimbCommand;
 
-public class ClimbSubsytem extends Subsystem {
+public class ClimberSubsystem extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    //Motors
+    private DoubleSolenoid solenoidFront;
+    private DoubleSolenoid solenoidBack;
 
-
-    public void initDefaultCommand() {
-        // TODO: Set the default command, if any, for a subsystem here. Example:
-        //    setDefaultCommand(new MySpecialCommand());
+    public ClimberSubsystem(){
+        solenoidFront = new DoubleSolenoid(RobotMap.climbFront_sol_fwd,RobotMap.climbFront_sol_rev);
+        solenoidBack = new DoubleSolenoid(RobotMap.climbBack_sol_fwd,RobotMap.climbBack_sol_rev);
     }
-}
 
+}
