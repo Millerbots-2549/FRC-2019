4d8a736d9c1925c82e21b1d0962cd55fd94601e1
viclabV1 <vplabrie@gmail.com>
2019-02-02 08:56:26 -0600

    ooga

--- src/main/java/frc/robot/subsystems/ClimbSubsytem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsytem.java
@@ -17,22 +17,28 @@ public class ClimberSubsystem extends Subsystem {
         solenoidBack = new DoubleSolenoid(RobotMap.climbBack_sol_fwd,RobotMap.climbBack_sol_rev);
     }
 
-    public void extend(){
+    public void extendFront(){
         solenoidFront.set(DoubleSolenoid.Value.kReverse);
-        solenoidBack.set(DoubleSolenoid.Value.kReverse);
         System.out.println("Climb initialized");
     }
 
-    public void retract(){
+    public void extendBack() {
+        solenoidBack.set(DoubleSolenoid.Valu.kReverse);
+    }
+
+
+    public void retractFront(){
         solenoidFront.set(DoubleSolenoid.Value.kForward);
-        solenoidBack.set(DoubleSolenoid.Value.kForward);
         System.out.println("Climb Finishing");
     }
 
+    public void retractBack(){
+        solenoidBack.set(DoubleSolenoid.Value.kForward);
+    }
     public void solenoidsOff(){
         solenoidFront.set(DoubleSolenoid.Value.kOff);
         solenoidBack.set(DoubleSolenoid.Value.kOff);
-        System.out.println("Climb Complete");
+        System.out.println("Climb Complete");-
     }
     )
 }
